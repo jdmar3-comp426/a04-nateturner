a80280d6a34a4b9593411e12f9dff04a4ca263f9
n8turner-unc <54559270+n8turner-unc@users.noreply.github.com>
2021-11-17 11:10:57 -0500

    a04 edit #1 app.post

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.get("/app/", (req, res, next) => {
 
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
-app.post("/app/new", (req, res) => {	
-	const stmt = db.prepare("INSERT into userinfo (user, pass) VALUES ("+req.body.user+", "+req.body.pass+")").all();
+app.post("/app/new/", (username, password) => {	
+	const stmt = db.prepare("INSERT into userinfo (user, pass) VALUES ("+username+", "+password+")").all();
 	res.status(201).json(stmt);
 });
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
