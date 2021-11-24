8c7c405c170f860df83117aaba1b76995c636ec3
n8turner-unc <54559270+n8turner-unc@users.noreply.github.com>
2021-11-24 13:04:43 -0500

    change #14 added md5 to password in update

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get("/app/user/:id", (req, res) => {
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {	
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?");
-	const u = stmt.run(req.body.user, req.body.pass, req.params.id);
+	const u = stmt.run(req.body.user, md5(req.body.pass), req.params.id);
 	res.status(405).json(u);
 });
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
